a0197fa3f118a83c5bbf4f6dbeb7c30dbcee0d2f
Aloy Aditya Sen <aloysen951@gmail.com>
2019-01-28 09:38:54 +0530

    initial commit
    this is non production

---
+++ modify.md
@@ -0,0 +1,23 @@
+### why are you here 
+
+you are required to fill in this data sheet everytime you log a succesful build of the engine 
+
+add the jist of the modifications in the new release version of the codebase so that the promotional team can work out with ashort teaser quite soon
+
+
+20 december 2018
+=================
+
+versio 4 beta relaease added
+- support for talent acquisation
+- home screen created
+
+what is missing
+===============
+publucations, academics, events, individual project pages 
+change of theme color to yellow
+
+
+07.01.2019
+============
+adding mars maven project to educational commute
---
+++ webpack.config.js
@@ -0,0 +1,102 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
+// We need Nodes fs module to read directory contents
+const fs = require('fs')
+
+var sassExtractor= new ExtractTextPlugin({
+   filename: 'main.css'
+});
+var styleExtractor = new ExtractTextPlugin({
+    filename: 'styles.css'
+})
+//product charts of the education line 
+var entry1 = [
+    'CommingSoon'
+  ];
+   // Call our function on our views directory.
+var entryHtmlPlugins = entry1.map(function (entryName) {
+    return new HtmlWebpackPlugin({
+      filename: entryName + '.html',
+      template: __dirname + `/src/raw-pages/${entryName}.html`,
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        conservativeCollapse: true
+      },
+    })
+  });
+  
+ 
+
+module.exports = {
+    entry: './src/js/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+        // publicPath: '/dist'     module loadding exceptions
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude:path.resolve(__dirname, "node_modules"),
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['es2015']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: sassExtractor.extract({
+                    use: ['css-loader', 'sass-loader']
+                })
+            },
+            {
+                test: /\.css$/,
+                use: styleExtractor.extract({
+                    fallback: 'style-loader',
+                    use: 'css-loader' 
+                })
+                
+            },
+            {
+                test: /\.(png|jp(e*)g|svg)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'img/',
+                            publicPath: 'img/'
+                        }
+                    }
+                ]
+            }
+    
+        ]
+    },
+    plugins: [
+        sassExtractor,
+        styleExtractor,
+        new CopyWebpackPlugin([ 
+            { from: 'src/img', to: 'images',toType:"dir", force: true },
+        ]),
+        new HtmlWebpackPlugin({
+            template: 'src/index.html',
+            minify: {
+                removeComments: true,
+                collapseWhitespace: true,
+                conservativeCollapse: true
+              },
+        }),
+        new CleanWebpackPlugin(['dist'])
+    ].concat(entryHtmlPlugins)
+};
